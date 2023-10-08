54ffa57e82b1fd5083f4297919a94bc3b21fa29a
rahal <rahalnoussayerr@gmail.com>
2023-10-08 14:47:49 +0100

    add

---
+++ js/day2/Practice/script.js
@@ -0,0 +1,7 @@
+console.log("page loaded...");
+function play() {
+    var x = document.getElementById("myVideo").play;
+  }
+function pause() {
+    var x = document.getElementById("myVideo").pause;
+  }
